c82773363f95c9e328076b0cbe6dc23caac8322d
Varun Kumar <varun.kumar12aug@gmail.com>
2025-09-21 12:13:13 +0530

    updated nat gateway from private to public

--- modules/vpc/nat-gateway.tf
+++ modules/vpc/nat-gateway.tf
@@ -11,7 +11,7 @@ resource "aws_nat_gateway" "k8s-nat-gateway" {
     count          = length(var.private_subnet_cidrs)
     depends_on     = [ aws_eip.k8s-nat-eip ]
     allocation_id  = aws_eip.k8s-nat-eip[count.index].id
-    subnet_id      = aws_subnet.k8s-private-subnet[count.index].id
+    subnet_id      = aws_subnet.k8s-public-subnet[count.index].id
     tags = {
         Name = "Private NAT-Gateway"
         description = "This is NAT Gateway for private subnet"
