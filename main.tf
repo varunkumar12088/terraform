77499def63328f86540b64015726e6d977aba588
Varun Kumar <varun.kumar12aug@gmail.com>
2025-09-21 09:32:14 +0530

    Removed

--- main.tf
+++ main.tf
@@ -3,10 +3,11 @@ module "vpc" {
   source = "./modules/vpc"
 }
 
-# ec2 module
 
-module "ec2" {
-  source = "./modules/ec2"
-  vpc_id = module.vpc.vpc_id
-  private_subnet_ids = module.vpc.private_subnet_ids  
-}
+
+# ec2 module
+#module "ec2" {
+#  source = "./modules/ec2"
+#  vpc_id = module.vpc.vpc_id
+#  private_subnet_ids = module.vpc.private_subnet_ids  
+#}
