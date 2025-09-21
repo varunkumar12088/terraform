6c3a4960206c5fca7f4fa12092f08be8ed165696
Varun Kumar <varun.kumar12aug@gmail.com>
2025-09-21 11:41:57 +0530

    added

--- modules/ec2/iam-role.tf
+++ modules/ec2/iam-role.tf
@@ -1,5 +1,5 @@
 
-resource "aws_iam_role" "ec2-ssm-role" {
+resource "aws_iam_role" "ec2_ssm_role" {
     name = "ec2-ssm-role"
     assume_role_policy = jsonencode({
         Version = "2012-10-17"
@@ -18,13 +18,13 @@ resource "aws_iam_role" "ec2-ssm-role" {
 
 
 resource "aws_iam_role_policy_attachment" "ssm_attach" {
-    role       = aws_iam_role.ec2-ssm-role.name
+    role       = aws_iam_role.ec2_ssm_role.name
     policy_arn = var.policy_arn
   
 }
 
 resource "aws_iam_instance_profile" "ec2_ssm" {
     name = "ec2-ssm-instance-profile"
-    role = aws_iam_role.ec2-ssm-role.name
+    role = aws_iam_role.ec2_ssm_role.name
   
 }
