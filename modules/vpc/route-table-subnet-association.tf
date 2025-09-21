b2d3d4a7dc3451adfb7b7115299079259d66d06f
Varun Kumar <varun.kumar12aug@gmail.com>
2025-09-21 10:26:21 +0530

    Removed some

--- modules/vpc/route-table-subnet-association.tf
+++ modules/vpc/route-table-subnet-association.tf
@@ -6,3 +6,9 @@ resource "aws_route_table_association" "public-rt-assoc" {
     route_table_id = aws_route_table.k8s-public-rt.id  
 }
 
+resource "aws_route_table_association" "private-rt-assoc" {
+    count          = length(var.private_subnet_cidrs)
+    depends_on     = [ aws_subnet.k8s-private-subnet, aws_route_table.k8s-private-rt ]
+    subnet_id      = aws_subnet.k8s-private-subnet[count.index].id
+    route_table_id = aws_route_table.k8s-private-rt.id  
+}
