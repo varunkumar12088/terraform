c402d06a5b0e9e15f4b2c21dba1f95fe86a7f56b
Varun Kumar <varun.kumar12aug@gmail.com>
2025-09-21 10:46:20 +0530

    added for ec2

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ module "vpc" {
 
 
 # ec2 module
-#module "ec2" {
-#  source = "./modules/ec2"
-#  vpc_id = module.vpc.vpc_id
-#  private_subnet_ids = module.vpc.private_subnet_ids  
-#}
+module "ec2" {
+  source = "./modules/ec2"
+  vpc_id = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids  
+}
